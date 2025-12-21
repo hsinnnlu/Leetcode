0c6a0cf423c550d92302894c6223d4a078384fe3
hsinnnlu <w133846664@gmail.com>
2025-12-21 23:57:07 +0800

    Day3 955

---
+++ Medium/Day3_955.py
@@ -0,0 +1,34 @@
+from typing import List
+
+def minDeletionSize(self, strs: List[str]) -> int:
+    n = len(strs)
+    m = len(strs[0])
+
+    resolved = [False] * (n - 1)
+    unresolved = n - 1
+    deletions = 0
+
+    for col in range(m):
+        if unresolved == 0:
+            break
+
+        bad = False
+        for i in range(n - 1):
+            if not resolved[i] and strs[i][col] > strs[i + 1][col]:
+                bad = True
+                break
+
+        if bad:
+            deletions += 1
+            continue
+
+        for i in range(n - 1):
+            if not resolved[i] and strs[i][col] < strs[i + 1][col]:
+                resolved[i] = True
+                unresolved -= 1
+
+    return deletions
+
+if __name__ == "__main__":
+    strs = ["xga", "xfb", "yfa"]
+    print(minDeletionSize(strs))
